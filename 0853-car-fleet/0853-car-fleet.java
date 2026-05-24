b22e9078e3aba430e7df040ed61560f0bc4e4a28
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-05-24 20:50:18 +0300

    Time: 97 ms (20.59%), Space: 147.9 MB (5.29%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Map<Integer,Double> m = new HashMap<>();
+        Map<Integer,Integer> m2 = new HashMap<>();
+        for(int i = 0; i < position.length; i++){
+            double time = 1.0 * (target - position[i]) / speed[i];
+            m.put(position[i],time);
+            m2.put(position[i],speed[i]);
+        }
+        Arrays.sort(position);
+        Deque<Integer> s = new ArrayDeque<>();
+        for(int i = position.length - 1; i >= 0; i--){
+            if(i == position.length-1) s.push(position[i]);
+            else{
+                if(m.get(position[i]) <= m.get(s.peek())) {
+                    if(m2.get(position[i]) < m2.get(s.peek())){
+                        s.pop();
+                        s.push(position[i]);
+                    }
+                } else {
+                    s.push(position[i]);
+                }   
+            }
+        }
+        return s.size();
+    }
+}
