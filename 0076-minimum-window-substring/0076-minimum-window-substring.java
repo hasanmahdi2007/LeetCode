da60850c6686d5898534288f344242e6aeacdcbc
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-05-26 23:54:44 +0300

    Time: 99 ms (14.61%), Space: 47.2 MB (46.53%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -5,38 +5,48 @@ class Solution {
         if(n2 > n1) return "";
         int[] letters = new int[52];
         int[] s_letters = new int[52];
+        int unique = 0;
         for(int i = 0; i < n2; i++){
             char c = t.charAt(i);
-            if(c >= 'a') letters[c - 'a' + 26]++;
-            else letters[c - 'A']++;
+            if(c >= 'a') {
+                if(letters[c - 'a' + 26]++ == 0) unique++;
+            }    
+            else {
+                if(letters[c - 'A']++ == 0) unique++;
+            }    
         }
         int left = 0;
         int shortest = Integer.MAX_VALUE;
         String shortest_string = "";
+        int fit = 0;
         for(int i = 0; i < n1; i++){
             char c = s.charAt(i);
-            if(c >= 'a') s_letters[c - 'a' + 26]++;
-            else s_letters[c - 'A']++;
-            while(fits(s_letters, letters) && left <= i){
+            if(c >= 'a') {
+                s_letters[c - 'a' + 26]++;
+                if(s_letters[c - 'a' + 26] == letters[c - 'a' + 26]) fit++;
+            }    
+            else {
+                s_letters[c - 'A']++;
+                if(s_letters[c - 'A' ] == letters[c - 'A']) fit++;
+            }    
+            while(unique == fit && left <= i){
                 int length = i - left + 1;
                 if(length < shortest) {
                     shortest = length;
                     shortest_string = s.substring(left,i+1);
                 }
                 char current = s.charAt(left);
-                if(current >= 'a') s_letters[current - 'a' + 26]--;
-                else s_letters[current - 'A']--;
+                if(current >= 'a') {
+                    s_letters[current - 'a' + 26]--;
+                    if(s_letters[current - 'a' + 26] < letters[current - 'a' + 26]) fit--;
+                }    
+                else {
+                    s_letters[current - 'A']--;
+                    if(s_letters[current - 'A'] < letters[current - 'A']) fit--;
+                }    
                 left++;
             } 
         }
         return shortest_string;
     }
-
-    public boolean fits(int[] s_letters, int[] letters){
-        for(int i = 0; i < 52; i++){
-            if(letters[i] > s_letters[i] && letters[i] != 0) return false;
-        }
-        return true;
-    }
-
 }
