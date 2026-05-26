62ad9b2935cf8436de1d5d10485f1f4f3c14d18f
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-05-26 06:35:48 +0300

    Time: 6 ms (86.56%), Space: 43.6 MB (96.51%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,35 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        int[] s1_letters = new int[26];
         int n1 = s1.length();
-        int n2 = s2.length();
-        if (n1 > n2) return false;
-        int[] s1Counts = new int[26];
-        int[] s2Counts = new int[26];
-        for (int i = 0; i < n1; i++) {
-            s1Counts[s1.charAt(i) - 'a']++;
-            s2Counts[s2.charAt(i) - 'a']++;
+        for(int i = 0; i < s1.length(); i++) {
+            s1_letters[s1.charAt(i) - 'a']++;
         }
-        int matches = 0;
-        for (int i = 0; i < 26; i++) {
-            if (s1Counts[i] == s2Counts[i]) matches++;
-        }
-        for (int i = n1; i < n2; i++) {
-            if (matches == 26) return true;
-            int rightIdx = s2.charAt(i) - 'a';
-            int leftIdx = s2.charAt(i - n1) - 'a';
-            s2Counts[rightIdx]++;
-            if (s2Counts[rightIdx] == s1Counts[rightIdx]) {
-                matches++; 
-            } else if (s2Counts[rightIdx] == s1Counts[rightIdx] + 1) {
-                matches--; 
+        int start = 0;
+        for(int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
+            int asci = c - 'a';
+            if(s1_letters[asci] != 0){
+                s1_letters[asci]--;
+                if(i - start + 1 == n1) return true;
             }
-            s2Counts[leftIdx]--;
-            if (s2Counts[leftIdx] == s1Counts[leftIdx]) {
-                matches++;
-            } else if (s2Counts[leftIdx] == s1Counts[leftIdx] - 1) {
-                matches--;
+            else {  
+                while(start < i){
+                    s1_letters[s2.charAt(start) - 'a']++;
+                    if(s1_letters[asci] > 0){
+                        s1_letters[asci] = 0;
+                        break;
+                    }
+                    start++; 
+                }
+                start++;
             }
-        }
-        return matches == 26;
+        }    
+        return false;
     }
 }
