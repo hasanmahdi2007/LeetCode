4f78f74e62b98c3940c6afdf63e8c5391cb671c1
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-06-05 00:47:43 +0300

    Time: 27 ms (74.37%), Space: 51.6 MB (94.34%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,19 +1,27 @@
 class Solution {
 
-    
+    private class Node{
+        public int index;
+        public int distance;
+
+        public Node(int distance, int index){
+            this.index = index;
+            this.distance = distance;
+        }
+    }
     public int[][] kClosest(int[][] points, int k) {
         int[][] answer = new int[k][2];
-        PriorityQueue<int[]> heap = new PriorityQueue<>(k, (a,b) -> Integer.compare(b[0], a[0]));
+        PriorityQueue<Node> heap = new PriorityQueue<>(k, (a,b) -> Integer.compare(b.distance, a.distance));
         for(int i = 0; i < points.length; i++){
             int distance = distance(points[i]);
             if(heap.size() == k){
-                if(distance < heap.peek()[0]){
+                if(distance < heap.peek().distance){
                     heap.poll();
-                    heap.add(new int[] {distance, i});
+                    heap.add(new Node(distance, i));
                 }
-            } else heap.add(new int[] {distance, i});
+            } else heap.add(new Node(distance, i));
         }
-        for(int i = 0; i < k; i++) answer[i] = points[heap.poll()[1]];
+        for(int i = 0; i < k; i++) answer[i] = points[heap.poll().index];
         return answer;
     }
 
