c785f7cb4646a20ba75c618443516c195affa87f
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-06-07 16:04:47 +0300

    Time: 3 ms (23.71%), Space: 47.8 MB (60.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,8 +3,8 @@ class Solution {
         if(nums[0] == 0 && nums.length != 1) return false;
         int steps = nums[0] - 1;
         for(int i = 1; i < nums.length-1; i++){
-            if(steps <= 0) return false;
             steps = Math.max(steps, nums[i]);
+            if(steps <= 0) return false;
             steps--;
         }
         return true;
