c440118cbb589464a6473972ed6863f7275999c7
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-06-07 15:59:16 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums[0] == 0 && nums.length != 1) return false;
+        int steps = nums[0] - 1;
+        for(int i = 1; i < nums.length-1; i++){
+            if(steps <= 0) return false;
+            steps = Math.max(steps, nums[i]);
+            steps--;
+        }
+        return true;
+    }
+}
