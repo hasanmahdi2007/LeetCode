ee14e8c8e490c1d646e075bce711213783ebfa21
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-06-12 17:35:01 +0300

    Time: 16 ms (21.9%), Space: 46.3 MB (60.8%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -2,40 +2,35 @@ class Solution {
     public int swimInWater(int[][] grid) {
         int n = grid[0].length;
         int m = grid.length;
-        int[][] bestTime = new int[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                bestTime[i][j] = Integer.MAX_VALUE;
-            }
-        }
+        boolean[][] visited = new boolean[m][n];
         PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> Integer.compare(a[2], b[2]));
         heap.offer(new int[] {0,0,grid[0][0]});
         while(true){
             int[] coordinates = heap.poll();
             int i = coordinates[0];
             int j = coordinates[1];
-            if(bestTime[i][j] <= coordinates[2]) continue;
-            bestTime[i][j] = coordinates[2];
+            if(visited[i][j]) continue;
+            visited[i][j] = true;
             int minTime = 0;
             if(i == m-1 && j == n-1) return coordinates[2];
             if(i != m-1) {
                 minTime = Math.max(coordinates[2], grid[i+1][j]);
-                if(minTime < bestTime[i+1][j])
+                if(!visited[i+1][j])
                     heap.offer(new int[] {i+1, j, minTime});
             }    
             if(i != 0) {
                 minTime = Math.max(coordinates[2], grid[i-1][j]);
-                if(minTime < bestTime[i-1][j])
+                if(!visited[i-1][j])
                     heap.offer(new int[] {i-1, j, minTime});
             }    
             if(j != n-1) {
                 minTime = Math.max(coordinates[2], grid[i][j+1]);
-                if(minTime < bestTime[i][j+1])
+                if(!visited[i][j+1])
                     heap.offer(new int[] {i, j+1, minTime});
             }   
             if(j != 0) {
                 minTime = Math.max(coordinates[2], grid[i][j-1]);
-                if(minTime < bestTime[i][j-1])
+                if(!visited[i][j-1])
                     heap.offer(new int[] {i, j-1, minTime});
             }    
         }
