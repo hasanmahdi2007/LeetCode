a965a2abbdf4d3581d448d1bf07e4815642b19a5
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-06-12 15:06:15 +0300

    Time: 1322 ms (5.01%), Space: 129.4 MB (19.29%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -4,24 +4,19 @@ class Solution {
         boolean[] visited = new boolean[points.length];
         int counter = 0;
         PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> a[0] - b[0]);
-        int pointind = 0;
-        do{
-            int[] point = points[pointind];
-            visited[pointind] = true;
+        heap.offer(new int[] {0,0});
+        while (!heap.isEmpty()){
+            int[] value = heap.poll();
+            int[] point = points[value[1]];
+            if(visited[value[1]]) continue;
+            visited[value[1]] = true;
             for(int j = 0; j < points.length; j++){
                 int[] point2 = points[j];
                 int distance = Math.abs(point[0] - point2[0]) + Math.abs(point[1] - point2[1]);
                 heap.offer(new int[] {distance, j});
             }
-            int[] value = heap.poll();
-            while(visited[value[1]]){
-                if(heap.isEmpty()) break;
-                value = heap.poll();
-            }
-            if(heap.isEmpty() && visited[value[1]]) break;
             counter += value[0];
-            pointind = value[1];
-        } while (!heap.isEmpty());
+        } 
         return counter;
     }
 }
