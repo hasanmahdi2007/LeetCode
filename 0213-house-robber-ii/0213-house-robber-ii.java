a99e876d2bca3fb4370d1488cd15396994f2680b
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-06-13 23:14:54 +0300

    Time: 0 ms (100%), Space: 42.6 MB (76.91%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,29 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
-        int[] memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        int withFirst = firstPath(0, nums, memo);
-        Arrays.fill(memo, -1);
-        int withLast = secondPath(1, nums, memo);
+        int withFirst = BackToTop(0,  nums.length-1, nums);
+        int withLast = BackToTop(1, nums.length, nums);
         return Math.max(withFirst, withLast);
     }
 
-    public int firstPath(int i, int[] nums, int[] memo){
-        if(i >= nums.length-1) return 0;
-        if(memo[i] != -1) return memo[i];
-        int didntTake = firstPath(i+1, nums, memo);
-        int took = nums[i] + firstPath(i+2, nums, memo);
-        memo[i] = Math.max(didntTake, took);
-        return memo[i];
-    }
-
-    public int secondPath(int i, int[] nums, int[] memo){
-        if(i >= nums.length) return 0;
-        if(memo[i] != -1) return memo[i];
-        int didntTake = secondPath(i+1, nums, memo);
-        int took = nums[i] + secondPath(i+2, nums, memo);
-        memo[i] = Math.max(didntTake, took);
-        return memo[i];
+    public int BackToTop(int start, int end, int[] nums){
+        int maxValue = 0;
+        int robberOne = 0;
+        int robberTwo = 0;
+        for(int i = start; i < end; i++){
+            maxValue = Math.max(robberTwo, nums[i] + robberOne);
+            robberOne = robberTwo;
+            robberTwo = maxValue;
+        }
+        return maxValue;
     }
 }
