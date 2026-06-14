4371093be48c09c0249092f3b9fa1d83f1ac2921
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-06-14 19:05:15 +0300

    Time: 14 ms (89.35%), Space: 46.4 MB (48.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,15 +1,11 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) return 0;
         int[] dp = new int[amount+1];
         Arrays.fill(dp, amount+1);
+        dp[0] = 0;
         for(int i = 1; i <= amount; i++){
             for(int coin : coins){
-                if(coin == i) {
-                    dp[i] = 1;
-                    break;
-                }
-                if(coin < i){
+                if(coin <= i){
                     if(dp[i - coin] + 1 < dp[i]) dp[i] = dp[i - coin] + 1;
                 }
             }
