00c1a77e1f3b35ee47a80825e729435befe459ea
Hasan Mahdi <hasan.mahdi7227@gmail.com>
2026-06-17 15:36:49 +0300

    Time: 8 ms (89.37%), Space: 47.1 MB (64.62%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -3,29 +3,25 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         int[][] dp = new int[m][n];
-        boolean[][] visited = new boolean[m][n];
         int max = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                int length = dfs(i, j, dp, matrix, visited, -1);
+                int length = dfs(i, j, dp, matrix, -1);
                 max = Math.max(max, length);
             }
         }
         return max;
     }
 
-    public int dfs(int i, int j, int[][] dp, int[][] matrix, boolean visited[][], int last){
-        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || visited[i][j]) 
-        return 0;
+    public int dfs(int i, int j, int[][] dp, int[][] matrix, int last){
+        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) return 0;
         if(matrix[i][j] <= last) return 0;
         if(dp[i][j] != 0) return dp[i][j];
-        visited[i][j] = true;
-        int left = dfs(i, j-1, dp, matrix, visited, matrix[i][j]);
-        int right = dfs(i, j+1, dp, matrix, visited, matrix[i][j]);
-        int up = dfs(i-1, j, dp, matrix, visited, matrix[i][j]);
-        int down = dfs(i+1, j, dp, matrix, visited, matrix[i][j]);
+        int left = dfs(i, j-1, dp, matrix, matrix[i][j]);
+        int right = dfs(i, j+1, dp, matrix, matrix[i][j]);
+        int up = dfs(i-1, j, dp, matrix, matrix[i][j]);
+        int down = dfs(i+1, j, dp, matrix, matrix[i][j]);
         dp[i][j] = Math.max(Math.max(Math.max(down, up), left), right) + 1;
-        visited[i][j] = false;
         return dp[i][j];
     }
 }
